3633ce73c429a1419a14a0ae6c963af1c771429b
3ddm3g <42741967+3ddm3g@users.noreply.github.com>
2018-09-12 22:15:09 +0200

    v1.2

--- dingdong.py
+++ dingdong.py
@@ -1,61 +1,45 @@
 #!/usr/bin/env python
 
-import sys, os, time, socket
-from playsound import playsound
+import sys, os
+from threading import Timer
 
-for k in range(1, 3):
-    if not sys.argv[k]:
-        print "usage: python dingdong.py [network] [start] [end]"
-        print "example: python dingdong.py 192.168.1 0 255"
-        sys.exit()
-
-if int(sys.argv[2]) > 255 or int(sys.argv[3]) > 255 or int(sys.argv[2]) < 0 or int(sys.argv[3]) < 0:
-    print "start end max 0 - 255"
-    for k in range(2, 4): print sys.argv[k]+" "+str(k)
+if not sys.argv[1]:
+    print "usage: python dingdong.py [ip range]"
+    print "example: python dingdong.py 192.168.0-255"
     sys.exit()
-else:
-    try: socket.inet_aton(sys.argv[1])
-    except socket.error:
-        print "szar network ip"
-        sys.exit()
 
-network = sys.argv[1]
-check_range = [int(sys.argv[2]), int(sys.argv[3])]
-refresh = 0
-
+iprange = sys.argv[1]
+network = iprange.split(".")[0]+"."+iprange.split(".")[1]+"."+iprange.split(".")[2]
+hostrange = [int(iprange.split(".")[3].split("-")[0]), int(iprange.split(".")[3].split("-")[1])]
 hosts = []
-online = []
-def target(lowtres, hightres):
-    for k in range(lowtres, hightres+1):
-        hosts.append(network + "." + str(k))
 
-def ping(host): return os.system("ping -c 1 "+host+" 2>&1 >/dev/null")
+class Host:
+    def __init__(self, cim, upno, uje):
+        self.ip = cim
+        self.online = upno
+        self.new = uje
+    def ping(self): return os.system("ping -c 1 "+self.ip+" 2>&1 >/dev/null")
+    def chnew(self, state): self.new = state
 
-def setInterval(func,time):
-    e = threading.Event()
-    while not e.wait(time):
-        func()
-
-#
-#os.clear()
-print "starting dingdong.py v1.0"
-print "targeting selected hosts... ["+sys.argv[1]+"."+sys.argv[2]+"-"+sys.argv[3]+"]"
-target(check_range[0], check_range[1])
-#for k in range(0, len(hosts)-1): print hosts[k]
-print "listening for connections..."
+for k in range(0,100): print(" ")
+print "starting dingdong.py v1.2"
+print "targeting selected hosts... [ "+iprange+" ]"
+for k in range(hostrange[0], hostrange[1]+1): hosts.append(Host(network + "." + str(k), False, True))
+print "listening on "+str(len(hosts))+" host(s)"
 while True:
     for k in range(0, len(hosts)-1):
-        feva = ping(hosts[k])
-        if hosts[k] in online:
-            if feva != 0:
-                online.remove(hosts[k])
-                print "[info] host disconnected: " + hosts[k]
-		os.system("omxplayer -p -o local Guap.mp3 2>&1 >/dev/null")
-                print "online: "+str(len(online))+" | offline: "+str(len(hosts)-len(online))
-                #for k in range(0, len(hosts)-1): print hosts[k] + " | " + online[k]
-        elif feva == 0:
-            online.append(hosts[k])
-            print "[info] greeting new host: " + hosts[k]
+        isup = hosts[k].ping()
+        if hosts[k].online:
+            if isup != 0:
+                avc = 0
+                hosts[k].online = False
+                print "[info] host disconnected: " + hosts[k].ip
+                for i in range(0, len(hosts)-1):
+                    if hosts[i].online: avc+=1
+                print "online: "+str(avc)+" | offline: "+str(len(hosts)-avc)
+        elif isup == 0 and hosts[k].new:
+            hosts[k].online = True
+            hosts[k].chnew(True)
+            print "[info] greeting new host: " + hosts[k].ip
+            Timer(3000, hosts[k].chnew(False)).start()
             os.system("omxplayer -p -o local Hering.mp3 2>&1 >/dev/null")
-	    if refresh > 0: time.sleep(refresh)
-#
